ac96b79362eca23a22d7f33f5e7d2e791127feb3
Carsten Könemann <carsten.koenemann@hmmh.de>
2017-04-24 10:36:05 +0200

    Initialize Xcode project

---
+++ POTesting/POTesting/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  POTesting
+//
+//  Created by Carsten Könemann on 24.04.2017.
+//  Copyright © 2017 Carsten Könemann. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
---
+++ POTesting/POTestingTests/POTestingTests.swift
@@ -0,0 +1,29 @@
+//
+//  POTestingTests.swift
+//  POTestingTests
+//
+//  Created by Carsten Könemann on 24.04.2017.
+//  Copyright © 2017 Carsten Könemann. All rights reserved.
+//
+
+import XCTest
+@testable import POTesting
+
+class POTestingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
